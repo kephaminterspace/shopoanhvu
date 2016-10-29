02ec223edbf943df28da568d1246c4504bf5ddcd
mebao <ke.v.pham@vntrip.vn>
2016-10-30 01:11:38 +0700

    the first

---
+++ templates/footer.php
@@ -0,0 +1,137 @@
+<?php
+	global $virtue_premium;
+
+	/**
+	* 
+	* @hooked kt_sitewide_shortcode_output - 10
+	* @hooked kt_sitewide_calltoaction_output - 20
+	*
+	*/
+	do_action('kt_before_footer');
+?>
+
+
+<footer id="containerfooter" class="footerclass">
+	<div class="container">
+		<ul class="footer-ser">
+			<li class="ser-gia-tot-nhat"><img src="<?php echo get_template_directory_uri()?>/assets/img/free/ser-gia-tot-nhat.png"></li>
+			<li class="ser-giao-hang"><img src="<?php echo get_template_directory_uri()?>/assets/img/free/ser-giao-hang.png"></li>
+			<li class="ser-doi-tra-hang"><img src="<?php echo get_template_directory_uri()?>/assets/img/free/ser-doi-tra-hang.png"></li>
+			<li class="ser-tu-van"><img src="<?php echo get_template_directory_uri()?>/assets/img/free/ser-tu-van.png"></li>
+			<li class="ser-ho-tro"><img src="<?php echo get_template_directory_uri()?>/assets/img/free/ser-ho-tro.png"></li>
+		</ul>
+		<div class="clearfix"></div>
+	</div>
+	<div class="container" style="border-top: 1px solid #ccc!important;">
+		<div class="row">
+
+			<div class="col-md-3 col-sm-6 footercol1">
+				<div class="widget-1 widget-first footer-widget">
+					<aside id="text-3" class="widget widget_text">
+						<h3>Hỗ chợ mua hàng</h3>
+						<div class="textwidget">
+                            <ul id="menu-resources" class="menu">
+                                <li class="menu-home menu-item-229">
+                                    <a href="http://yeulanhmanh.com/phuong-thuc-van-chuyen/">Phương thức vận chuyển</a>
+                                </li>
+                                <li class="menu-cart menu-item-230">
+                                    <a href="http://yeulanhmanh.com/giao-hang-va-thu-tien/">Giao hàng và thu tiền</a>
+                                </li>
+                                <li class="menu-contact-us menu-item-231">
+                                    <a href="http://yeulanhmanh.com/huong-dan-thanh-toan/">Hướng dẫn thanh toán</a>
+                                </li>
+                            </ul>
+							<div class="clearfix"></div>
+                        </div>
+					</aside>
+				</div>
+			</div>
+
+            <div class="col-md-3 col-sm-6 footercol3">
+                <div class="widget-1 widget-first footer-widget logo_menu">
+                    <aside id="kadence_image_grid-3" class="widget kadence_image_grid">
+                        <h3>Dịch vụ giao hàng</h3>
+                        <ul class="footer-ser">
+                            <li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/service/ship-ems.png"></li>
+                            <li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/service/ship-tasetco.png"></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </aside>
+
+					<aside id="nav_menu-4" class="widget widget_nav_menu" style="margin-top:0px;">
+						<h3>Hình thức thanh toán</h3>
+						<ul class="footer-ser">
+							<li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/payment/pay-tien-mat.png"></li>
+							<li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/payment/pay-atm.png"></li>
+							<li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/payment/pay-master.png"></li>
+							<li class="img_ser_pay"><img src="<?php echo get_template_directory_uri()?>/assets/img/payment/pay-visa.png"></li>
+						</ul>
+						<div class="clearfix"></div>
+					</aside>
+                </div>
+            </div>
+
+			<div class="col-md-3 col-sm-6 footercol2">
+				<div class="widget-1 widget-first footer-widget">
+					<aside id="widget_kadence_contact-2" class="widget widget_kadence_contact">
+						<h3>Liên hệ với chúng tôi</h3>
+						<div class="vcard">
+							<p><a href="javascript:void(0);"><i class="icon-phone"></i>&nbsp0979 - 617 - 634</a></p>
+							<p><a href="javascript:void(0);"><i class="icon-phone"></i>&nbsp01635 - 243 - 892</a></p>
+							<p><a href="javascript:void(0);"><i class="icon-tablet"></i>&nbsp718 Đường Láng - Đống Đa - Hà Nội</a></p>
+							<p><a href="javascript:void(0);"><i class="icon-folder"></i>&nbspThời gian bán hàng - 24/7</a></p>
+						</div>
+					</aside>
+				</div>
+				<div class="widget-2 widget-last footer-widget">
+					<aside id="widget_kadence_social-2" class="widget widget_kadence_social">
+						<h3>Mạng xã hội</h3>
+						<div class="virtue_social_widget clearfix">
+							<a href="https://www.facebook.com/Stud100chongxuattinhsomchinhhang/" class="facebook_link" title="" target="_blank" data-toggle="tooltip" data-placement="top" data-original-title="Facebook"><i class="icon-facebook"></i></a>
+							<a href="javascript:void(0);" class="youtube_link" title="" target="_blank" data-toggle="tooltip" data-placement="top" data-original-title="Youtube"><i class="icon-youtube"></i></a>
+							<a href="javascript:void(0);" class="google-plus_link" title="" target="_blank" data-toggle="tooltip" data-placement="top" data-original-title="Google-plus"><i class="icon-google-plus"></i></a>
+							<a href="javascript:void(0);" class="instagram_link" title="" target="_blank" data-toggle="tooltip" data-placement="top" data-original-title="Instagram"><i class="icon-instagram"></i></a>
+						</div>
+					</aside>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-6 footercol4">
+				<div class="widget-1 widget-first footer-widget">
+					<div class="fb-page" data-href="https://www.facebook.com/Stud100chongxuattinhsomchinhhang/" data-tabs="timeline" data-width="270" data-height="240" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Stud100chongxuattinhsomchinhhang/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Stud100chongxuattinhsomchinhhang/">Stud 100 - thuốc xịt kéo dài thời gian quan hệ</a></blockquote></div>
+				</div>
+			</div>
+
+		</div>
+		<div class="footercredits clearfix">
+			<div class="footernav clearfix">
+				<ul id="menu-resources-1" class="footermenu">
+					<li class="menu-home menu-item-229">
+						<a href="http://yeulanhmanh.com/">Trang chủ</a>
+					</li>
+					<li class="menu-cart menu-item-230">
+						<a href="http://yeulanhmanh.com/cart/">Giỏ hàng</a>
+					</li>
+					<li class="menu-contact-us menu-item-231">
+						<a href="http://yeulanhmanh.com/lien-va-gop-y-voi-yeu-lanh-manh/">Liên hệ với chúng tôi</a>
+					</li>
+					<li class="menu-my-account menu-item-232">
+						<a href="http://yeulanhmanh.com/my-account/">Tài khoản của bạn</a>
+					</li>
+				</ul>
+			</div>
+			<p>Bản quyền thuộc về <a href="http://shopmebao.vn/" target="_blank">ShopMeBao.Vn</a></p>
+		</div>
+	</div>
+</footer>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8&appId=477360942364792";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+<?php wp_footer(); ?>
